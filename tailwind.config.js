f1bf843c85b30a482b56759b5958fb16b98fdd7e
DRY-7717 <wicaksanabimaarya01@gmail.com>
2023-06-24 20:11:30 +0700

    test file tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./src/index.html"],
   theme: {
     extend: {},
     fontFamily: {
       poppins: ["Poppins"],
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
